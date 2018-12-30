c7a58b1a4ecdbb0cdf79888976b5a3cd9ea3b088
魏巍 <wewin11235@163.com>
2018-12-30 11:11:07 +0800

    feat(algorithm-test): test algorithm

--- checkInclusion.js
+++ checkInclusion.js
@@ -1,39 +1,52 @@
+/**
+ * @param {string} s1
+ * @param {string} s2
+ * @return {boolean}
+ */
 var checkInclusion = function(s1, s2) {
   if ( s1.length > s2.length ) return false;
-  var matched_indexs = {};
-  var matched_indexs_arr = []
-  for (var i = 0; i < s1.length; i++) {
-      var start_index = matched_indexs[s1[i]] ?  matched_indexs[s1[i]][-1] : -1;
-      var next_match_index = s2.indexOf(s1[i], start_index + 1);
-
-      if ( next_match_index === -1 ) return false;
-
-
-      if (matched_indexs[s1[i]]) {
-          matched_indexs[s1[i]].push(next_match_index);
+  var s1_char_count = {};
+  var s2_char_count = {};
+  for ( var i = 0; i < s1.length; i++ ) {
+      if ( s1_char_count[s1[i]] ) {
+          s1_char_count[s1[i]] ++;
       } else {
-          matched_indexs[s1[i]] = [next_match_index];
+          s1_char_count[s1[i]] = 1;
+      }
+
+      if ( s2_char_count[s2[i]] ) {
+          s2_char_count[s2[i]] ++;
+      } else {
+          s2_char_count[s2[i]] = 1;
       }
   }
 
-  console.log(matched_indexs);
-  for ( char in matched_indexs ) {
-      if ( matched_indexs.hasOwnProperty(char) ) {
-          matched_indexs_arr = matched_indexs_arr.concat(matched_indexs[char]);
+  if ( objEqual(s1_char_count, s2_char_count) ) return true;
+
+  var start_index = 0;
+  for ( var j = s1.length; j < s2.length; j ++ ) {
+      var char_start  = s2[start_index];
+      var chart_end = s2[j];
+      s2_char_count[char_start] --;
+      if ( s2_char_count[chart_end] ) {
+          s2_char_count[chart_end] ++;
+      } else {
+          s2_char_count[chart_end] = 1;
       }
+
+      if ( objEqual(s1_char_count, s2_char_count) ) return true;
+
+      start_index ++;
   }
 
-  if ( matched_indexs_arr.length !== s1.length ) return false;
-  var matched_indexs_arr_sort = matched_indexs_arr.sort();
-
-  return matched_indexs_arr_sort.every(is_increment_arr)
+  return false;
 };
 
-function is_increment_arr(e, index, arr) {
-  if( index === 0) return true;
-  return ( e - arr[index - 1] === 1 );
-}
 
-var s1 = 'adc';
-var s2 = 'dcda';
-console.log( checkInclusion(s1, s2) );
+function objEqual(obj1, obj2) {
+  for ( key in obj1 ) {
+      if ( obj1[key] !== obj2[key]) return false;
+  }
+
+  return true
+}
