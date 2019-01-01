5b2bb32b53a34dbffc3c2e026d57f25e7c41d580
魏巍 <wewin11235@163.com>
2019-01-01 10:04:26 +0800

    feat(learn-algorithm): product to sumn

--- product_big_nums.js
+++ product_big_nums.js
@@ -20,13 +20,34 @@ var multiply = function(num1, num2) {
 };
 
 var get_products_arr = function(num1, num2_arr) {
-  var sum_arr = [];
+  var sum_arr = [0];
   for (i = 1; i <= num1; i++) {
     sum_arr = get_sum_arr(sum_arr, num2_arr);
   }
+
+  return sum_arr;
 }
 
 // 大数相加
 var get_sum_arr = function(sum_arr, num2_arr) {
+  var len = sum_arr.length > num2_arr.length ? sum_arr.length : num2_arr.length;
+  for (var i = 0; i < len - sum_arr.length; i ++) {
+    sum_arr.unshift(0);
+  }
 
+  for (var i = 0; i < len - num2_arr.length; i ++) {
+    sum_arr.unshift(0);
+  }
+
+  var to_next = 0;
+  for (var i = len - 1; i >= 0; i --) {
+    var sum = Number(sum_arr[i]) + Number(num2_arr[i]) + to_next;
+    var tmp = sum % 10;
+    to_next = sum - tmp;
+    sum_arr[i] = tmp;
+  }
+
+  if (to_next > 0) sum_arr.unshift(to_next);
+
+  return sum_arr;
 }
